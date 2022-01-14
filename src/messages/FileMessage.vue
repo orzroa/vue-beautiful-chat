e00c1c7ad6d69120b88bcd5abbaed396e6e9a44a
sc <rminn>
2022-01-14 22:50:05 +0800

    FileMessage: no preview if not image, no text line if text is null

--- src/messages/FileMessage.vue
+++ src/messages/FileMessage.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="sc-message--file" :style="messageColors">
     <div class="sc-message--file-icon">
-      <img :src="data.file.url" class="sc-image" />
+      <img
+        v-if="imageExtension.has(data.file.url.split('?')[0].split('.').pop())"
+        :src="data.file.url"
+        class="sc-image"
+      />
     </div>
     <div class="sc-message--file-name" :style="messageColors">
       <a :href="data.file.url ? data.file.url : '#'" target="_blank">{{ data.file.name || '' }}</a>
     </div>
-    <div class="sc-message--file-text" :style="messageColors">
+    <div v-if="data.text" class="sc-message--file-text" :style="messageColors">
       {{ data.text }}
+      <p v-if="data.meta" class="sc-message--meta" :style="messageColors">
+        {{ data.meta }}
+      </p>
+    </div>
+    <div v-if="!data.text" :style="messageColors">
       <p v-if="data.meta" class="sc-message--meta" :style="messageColors">
         {{ data.meta }}
       </p>
@@ -25,6 +34,11 @@ export default {
     messageColors: {
       type: Object,
       required: true
+    }
+  },
+  data() {
+    return {
+      imageExtension: new Set(['gif', 'jpg', 'jpeg', 'png'])
     }
   }
 }
